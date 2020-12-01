5cc5788541f22859d51a4aad163cd4905501b2a6
astro_dev <isajaffarali@gmail.com>
2020-12-01 13:46:50 +0300

    removed url

--- server/routes/router.js
+++ server/routes/router.js
@@ -10,17 +10,17 @@ route.get('/', services.homeRoutes);
 
 // @description Route route
 // @method Get
-route.get('https://astrousers.herokuapp.com/add-user', services.add_user);
+route.get('/add-user', services.add_user);
 
 
 // @description Route route
 // @method Get
-route.get('https://astrousers.herokuapp.com/update-user', services.update_user);
+route.get('/update-user', services.update_user);
 
 // API
-route.post('https://astrousers.herokuapp.com/api/users/', controller.create);
-route.get('https://astrousers.herokuapp.com/api/users/', controller.find);
-route.put('https://astrousers.herokuapp.com/api/users/:id', controller.update);
-route.delete('https://astrousers.herokuapp.com/api/users/:id', controller.delete);
+route.post('/api/users/', controller.create);
+route.get('/api/users/', controller.find);
+route.put('/api/users/:id', controller.update);
+route.delete('/api/users/:id', controller.delete);
 
 module.exports = route;
